5c54d2502329ae1465baf63be7b9201e69a92fed
D'Amore Lorenza <lorenzadamore@outlook.it>
2023-10-18 11:34:14 +0200

    mod

---
+++ logInPage.js
@@ -0,0 +1,27 @@
+
+const accessoButton= document.getElementById("accesso")
+
+
+const formReference=document.getElementById("form")
+
+formReference.addEventListener("submit", function(e){
+
+e.preventDefault()
+
+const inputEmail=document.getElementById("exampleInputEmail1")
+const inputPassword=document.getElementById("exampleInputPassword1")
+
+const newLogIn = {
+
+email: inputEmail.value,
+password: inputPassword.value
+
+}
+
+const getLogIn = function({
+
+fetch()
+
+})
+
+})
